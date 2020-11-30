0209b808efeb60161f138acbabf7eadccadb8e9e
Aleksej579 <aleksej.eremenko579@gmail.com>
2020-11-30 02:02:46 +0200

    add menu+first block+footer: next step image cart.

--- my-project/gulpfile.js
+++ my-project/gulpfile.js
@@ -8,22 +8,36 @@ function pug2html(cb) {
     return (
         gulp
             .src('src/*.pug')
-            .pipe(pug())
+            .pipe(pug({
+                pretty: true
+            }))
             .pipe(gulp.dest('./dest'))
     );
-
 }
-
 function style() {
     return (
         gulp
             .src("src/sass/*.sass")
             .pipe(sass())
             .on("error", sass.logError)
+            .pipe(concat('styles.css'))
             .pipe(gulp.dest("./dest"))
     );
 }
-
+function fonts() {
+    return (
+        gulp
+            .src('src/fonts/**/*.{eot,svg,ttf,woff,woff2}')
+            .pipe(gulp.dest('./dest/fonts/'))
+    )
+}
+function images() {
+    return (
+        gulp
+            .src('src/images/**/*.{png, jpeg, svg}')
+            .pipe(gulp.dest('./dest/images/'))
+    )
+}
 function js() {
     return (
         gulp
@@ -33,13 +47,11 @@ function js() {
             .pipe(gulp.dest('./dest'))
     );
 }
-
 function watch() {
     gulp.watch('src/*.pug', pug2html)
+    gulp.watch('src/includes/*.pug', pug2html)
     gulp.watch('src/sass/*.sass', style)
     gulp.watch('src/js/*.js', js)
 }
 
-module.exports.start = gulp.series(pug2html, style, js, watch)
-
-
+module.exports.start = gulp.series(pug2html, style, js, fonts, images, watch)
