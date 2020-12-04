28083fc6c27e146b1ce062cacccce010fb713648
Aleksej579 <aleksej.eremenko579@gmail.com>
2020-12-04 02:44:49 +0200

    end

--- my-project/src/js/main.js
+++ my-project/src/js/main.js
@@ -5,3 +5,15 @@ const swiper = new Swiper('.swiper-container', {
     },
     loop: true,
 });
+
+
+$(".catr_btn").hover(
+    function () {
+        $(this).parent().find('.blue_header').css({ 'backgroundColor': '#b0b7ea' });
+        $(this).css({ 'backgroundColor': '#b0b7ea' });
+    },
+    function () {
+        $(this).parent().find('.blue_header').css({ 'backgroundColor': '#5b6ceb' });
+        $(this).css({ 'backgroundColor': '#5b6ceb' });
+    }
+);
